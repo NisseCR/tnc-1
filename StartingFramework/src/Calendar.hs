8897c2ed5956e9181a6eab280af95d064132af56
Thochril <tcloef@gmail.com>
2023-11-29 17:09:15 +0100

    Vraag 7, op naar die 10git add .!

--- StartingFramework/src/Calendar.hs
+++ StartingFramework/src/Calendar.hs
@@ -7,16 +7,16 @@ import DateTime
 
 
 -- Exercise 6
-data Calendar = Calendar {events :: [Event]
-                         , calProps :: [CalProp]}
-    deriving (Eq, Ord)
+data Calendar = Calendar {calProps :: [CalProp]
+                          , events :: [Event]}
+    deriving (Eq, Ord, Show)
 
 data CalProp = ProdId {prodId :: String}
              | Version {version :: String }
-    deriving (Eq, Ord)
+    deriving (Eq, Ord, Show)
 
 data Event = Event {eventProps :: [EventProp]}
-    deriving (Eq, Ord)
+    deriving (Eq, Ord, Show)
 
 data EventProp = DtStamp DateTime
                | Uid String
@@ -25,32 +25,35 @@ data EventProp = DtStamp DateTime
                | Description String
                | Summary String
                | Location String
-    deriving (Eq, Ord)
+    deriving (Eq, Ord, Show)
 
 -- Exercise 7
-data Token = TokenEmpty
-           | TCalProdId String
-           | TCalVersion String
-           | TEveDtStamp DateTime
-           | TEveUid String
-           | TEveDtStart DateTime
-           | TEveDtEnd DateTime
-           | TEveDescription String
-           | TEveSummary String
-           | TEveLocation String
+data Token = TokenText String
+           | TokenDateTime DateTime
+           | TokenWrapper
+           | TokenDtStamp
+           | TokenUid
+           | TokenDtStart
+           | TokenDtEnd
+           | TokenDescription
+           | TokenSummary
+           | TokenLocation
+           | TokenProdId
+           | TokenVersion
+           | TokenCrlf
     deriving (Eq, Ord, Show)
 
 scanCalendar :: Parser Char [Token]
-scanCalendar = greedy $ scanToken <* token "\r\n"
+scanCalendar =  concat <$> greedy (scanToken <* scanTokenBreak)
 
-scanToken :: Parser Char Token
-scanToken = scanEmptyToken "BEGIN:VCALENDAR"
-         <|> scanEmptyToken "END:VCALENDAR"
-         <|> scanEmptyToken "BEGIN:VEVENT"
-         <|> scanEmptyToken "END:VEVENT"
-         <|> scanDateTimeToken "DTSTAMP:"
-         <|> scanDateTimeToken "DTSTART:"
-         <|> scanDateTimeToken "DTEND:"
+scanTokenBreak :: Parser Char [Token]
+scanTokenBreak = const [TokenCrlf] <$> token "\r\n"
+
+scanToken :: Parser Char [Token]
+scanToken = scanWrapper "BEGIN:VCALENDAR"
+         <|> scanWrapper "END:VCALENDAR"
+         <|> scanWrapper "BEGIN:VEVENT"
+         <|> scanWrapper "END:VEVENT"
          <|> scanDateTimeToken "DTSTAMP:"
          <|> scanDateTimeToken "DTSTART:"
          <|> scanDateTimeToken "DTEND:"
@@ -61,27 +64,33 @@ scanToken = scanEmptyToken "BEGIN:VCALENDAR"
          <|> scanTextToken "VERSION:"
          <|> scanTextToken "PRODID:"
             
-scanEmptyToken :: String -> Parser Char Token
-scanEmptyToken prefix = f <$> token prefix 
-    where
-        f _ = TokenEmpty
+scanWrapper :: String -> Parser Char [Token]
+scanWrapper prefix = const [TokenWrapper] <$> token prefix 
 
 -- Datetime tokens
-scanDateTimeToken :: String -> Parser Char Token
+scanDateTimeToken :: String -> Parser Char [Token]
 scanDateTimeToken prefix = f <$> token prefix <*> parseDateTime
     where
         f _ datetime = assignDateTimeToken prefix datetime
 
-assignDateTimeToken :: String -> DateTime -> Token
-assignDateTimeToken "DTSTAMP:" result = TEveDtStamp result
-assignDateTimeToken "DTSTART:" result = TEveDtStart result
-assignDateTimeToken "DTEND:" result = TEveDtEnd result
+assignDateTimeToken :: String -> DateTime -> [Token]
+assignDateTimeToken "DTSTAMP:" result = [TokenDtStamp, TokenDateTime result, TokenCrlf]
+assignDateTimeToken "DTSTART:" result = [TokenDtStart, TokenDateTime result, TokenCrlf]
+assignDateTimeToken "DTEND:" result = [TokenDtEnd, TokenDateTime result, TokenCrlf]
 
 -- Text tokens
-scanTextToken :: String -> Parser Char Token
+scanTextToken :: String -> Parser Char [Token]
 scanTextToken prefix = f <$> token prefix <*> parseText
     where
         f _ text = assignTextToken prefix text
+
+assignTextToken :: String -> String -> [Token]
+assignTextToken "UID:" result = [TokenUid, TokenText result, TokenCrlf]
+assignTextToken "DESCRIPTION:" result = [TokenDescription, TokenText result, TokenCrlf]
+assignTextToken "SUMMARY:" result = [TokenSummary, TokenText result, TokenCrlf]
+assignTextToken "LOCATION:" result = [TokenLocation, TokenText result, TokenCrlf]
+assignTextToken "PRODID:" result = [TokenProdId, TokenText result, TokenCrlf]
+assignTextToken "VERSION:" result = [TokenVersion, TokenText result, TokenCrlf]
 
 parseText :: Parser Char String
 parseText = f <$> parseLines <*> parseLine
@@ -97,18 +106,48 @@ parseLine = greedy $ satisfy (\c -> c /= '\r')
 parseBreak :: Parser Char String
 parseBreak = token "\r\n "
 
-assignTextToken :: String -> String -> Token
-assignTextToken "UID:" result = TEveUid result
-assignTextToken "DESCRIPTION:" result = TEveDescription result
-assignTextToken "SUMMARY:" result = TEveSummary result
-assignTextToken "LOCATION:" result = TEveLocation result
-assignTextToken "PRODID:" result = TCalProdId result
-assignTextToken "VERSION:" result = TCalVersion result
-
 parseCalendar :: Parser Token Calendar
-parseCalendar = f <$> 
+--parseCalendar = f <$> (symbol TokenWrapper *> many parseCalendarProps) <*> many parseEvents <* symbol TokenWrapper 
+parseCalendar = pack (symbol TokenWrapper) parseCalendarContent (symbol TokenWrapper) 
+
+parseCalendarContent ::  Parser Token Calendar
+parseCalendarContent = f <$> many parseCalendarProp <*> many parseEvent
     where
         f props events = Calendar props events
+    
+parseCalendarProp :: Parser Token CalProp
+parseCalendarProp = ProdId <$> pack (symbol TokenProdId) (ttext) (symbol TokenCrlf)
+                 <|> Version <$> pack (symbol TokenVersion) (ttext) (symbol TokenCrlf)
+
+parseEvent :: Parser Token Event
+parseEvent = pack (symbol TokenWrapper) parseEventContent (symbol TokenWrapper) 
+
+parseEventContent ::  Parser Token Event
+parseEventContent = Event <$> many parseEventProp
+
+parseEventProp :: Parser Token EventProp
+parseEventProp = DtStamp <$> pack (symbol TokenDtStamp) (tdatetime) (symbol TokenCrlf)
+              <|> Uid <$> pack (symbol TokenUid) (ttext) (symbol TokenCrlf)
+              <|> DtStart <$> pack (symbol TokenDtStart) (tdatetime) (symbol TokenCrlf)
+              <|> DtEnd <$> pack (symbol TokenDtEnd) (tdatetime) (symbol TokenCrlf)
+              <|> Description <$> pack (symbol TokenDescription) (ttext) (symbol TokenCrlf)
+              <|> Summary <$> pack (symbol TokenSummary) (ttext) (symbol TokenCrlf)
+              <|> Location <$> pack (symbol TokenLocation) (ttext) (symbol TokenCrlf)
+
+ttext :: Parser Token String
+ttext = anySymbol >>= isTText
+  where
+    isTText :: Token -> Parser Token String
+    isTText (TokenText t) = succeed t
+    isTText _ = empty
+
+
+tdatetime :: Parser Token DateTime
+tdatetime = anySymbol >>= isTDateTime
+  where
+    isTDateTime :: Token -> Parser Token DateTime
+    isTDateTime (TokenDateTime dt) = succeed dt
+    isTDateTime _ = empty
 
 recognizeCalendar :: String -> Maybe Calendar
 recognizeCalendar s = run scanCalendar s >>= run parseCalendar
